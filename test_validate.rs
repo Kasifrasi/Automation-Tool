96f629643268a53067ca95d4c44ec79b4d27d3e8
Kasifrasi <73264146+Kasifrasi@users.noreply.github.com>
2026-03-24 21:27:28 +0100

    Delete unused tests

--- test_validate.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("{:?}", folder_generator::format_project_name("a 123"));
-    println!("{:?}", folder_generator::format_project_name("a 123_"));
-    println!("{:?}", folder_generator::format_project_name("2025_0004_003_"));
-}
